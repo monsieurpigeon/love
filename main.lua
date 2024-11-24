280728094f94f6cd44a78f40926538c3def8461c
Maxime PIGEON <maxpige@gmail.com>
2024-11-24 18:48:57 +0100

    images

--- main.lua
+++ main.lua
@@ -1,24 +1,23 @@
-local r1, r2
+local myImage, width, height, rotation
 
 function love.load()
-    Object = require "classic"
-    --We no longer need to require shape here.
-
-    local Rectangle = require "rectangle"
-
-    local Circle = require "circle"
-
-    r1 = Rectangle(100, 100, 200, 50)
-
-    r2 = Circle(350, 80, 40)
+    myImage = love.graphics.newImage("assets/sheep.png")
+    love.graphics.setBackgroundColor(1, 1, 1)
+    width = myImage:getWidth()
+    height = myImage:getHeight()
+    rotation = 0
 end
 
 function love.update(dt)
-    r1:update(dt)
-    r2:update(dt)
+    rotation = rotation + dt
 end
 
 function love.draw()
-    r1:draw()
-    r2:draw()
+    love.graphics.setColor(255 / 255, 200 / 255, 40 / 255, 127 / 255)
+    love.graphics.setColor(1, 0.78, 0.15, 0.5)
+    -- Or ...
+    love.graphics.draw(myImage, 100, 100, rotation, 1, 1, width / 2, height / 2)
+    -- Not passing an argument for alpha automatically sets it to 1 again.
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(myImage, 200, 100, rotation, 1, 1, width / 2, height / 2)
 end
