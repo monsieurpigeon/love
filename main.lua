820475c3ffddbea7b5e87c3533e566c4283d91cd
Maxime PIGEON <maxpige@gmail.com>
2024-11-24 18:55:52 +0100

    check collision

--- main.lua
+++ main.lua
@@ -1,23 +1,58 @@
-local myImage, width, height, rotation
+local r1, r2
 
 function love.load()
-    myImage = love.graphics.newImage("assets/sheep.png")
-    love.graphics.setBackgroundColor(1, 1, 1)
-    width = myImage:getWidth()
-    height = myImage:getHeight()
-    rotation = 0
+    --Create 2 rectangles
+    r1 = {
+        x = 10,
+        y = 100,
+        width = 100,
+        height = 100
+    }
+
+    r2 = {
+        x = 250,
+        y = 120,
+        width = 150,
+        height = 120
+    }
 end
 
 function love.update(dt)
-    rotation = rotation + dt
+    --Make one of rectangle move
+    r1.x = r1.x + 100 * dt
 end
 
 function love.draw()
-    love.graphics.setColor(255 / 255, 200 / 255, 40 / 255, 127 / 255)
-    love.graphics.setColor(1, 0.78, 0.15, 0.5)
-    -- Or ...
-    love.graphics.draw(myImage, 100, 100, rotation, 1, 1, width / 2, height / 2)
-    -- Not passing an argument for alpha automatically sets it to 1 again.
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.draw(myImage, 200, 100, rotation, 1, 1, width / 2, height / 2)
+    --We create a local variable called mode
+    local mode
+    if checkCollision(r1, r2) then
+        --If there is collision, draw the rectangles filled
+        mode = "fill"
+    else
+        --else, draw the rectangles as a line
+        mode = "line"
+    end
+
+    --Use the variable as first argument
+    love.graphics.rectangle(mode, r1.x, r1.y, r1.width, r1.height)
+    love.graphics.rectangle(mode, r2.x, r2.y, r2.width, r2.height)
+end
+
+function checkCollision(a, b)
+    --With locals it's common usage to use underscores instead of camelCasing
+    local a_left = a.x
+    local a_right = a.x + a.width
+    local a_top = a.y
+    local a_bottom = a.y + a.height
+
+    local b_left = b.x
+    local b_right = b.x + b.width
+    local b_top = b.y
+    local b_bottom = b.y + b.height
+
+    --Directly return this boolean value without using if-statement
+    return a_right > b_left
+        and a_left < b_right
+        and a_bottom > b_top
+        and a_top < b_bottom
 end
